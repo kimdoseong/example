935c2a9bb5ba55d5117734c8bf4f57af5c747b49
doseong.kim <doseong.kim@lgepartner.com>
2023-08-08 09:33:39 +0900

    Apply .gitignore

--- build/json/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/doseong/projects/json-cbor/json
-# Build directory: /home/doseong/projects/json-cbor/build/json
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("client")
-subdirs("server")
